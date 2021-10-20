370746124258d043d19aee714566d5b047564575
Yann Papouin <ypa@decgroupe.com>
2021-10-20 13:56:20 +0200

    [FIX] auditlog: Use dedicated cursor for autovacuum with «split and commit» to reduce cron worker timeouts

--- auditlog/models/autovacuum.py
+++ auditlog/models/autovacuum.py
@@ -3,8 +3,8 @@
 import logging
 from datetime import datetime, timedelta
 
-from odoo import models, fields, api
-
+from odoo import models, fields, api, registry
+from odoo.tools.misc import split_every
 
 _logger = logging.getLogger(__name__)
 
@@ -29,12 +29,27 @@ class AuditlogAutovacuum(models.TransientModel):
             'auditlog.http.request',
             'auditlog.http.session',
         )
+        # Create a new cursor to run this action
+        cr = registry(self._cr.dbname).cursor()
+        # Assign this cursor to self and all arguments to ensure consistent
+        # data in all method
+        self = self.with_env(self.env(cr=cr))
+
+        SPLIT = 10000
         for data_model in data_models:
             records = self.env[data_model].search(
-                [('create_date', '<=', fields.Datetime.to_string(deadline))])
-            nb_records = len(records)
-            records.unlink()
-            _logger.info(
-                "AUTOVACUUM - %s '%s' records deleted",
-                nb_records, data_model)
+                [('create_date', '<=', fields.Datetime.to_string(deadline))]
+            )
+            idx = 0
+            for ids in list(split_every(SPLIT, records.ids)):
+                _logger.info(
+                    '%s AUTOVACUUM (%d -> %d)/%d', data_model, idx,
+                    min(idx + SPLIT, len(records.ids)), len(records.ids)
+                )
+                idx += SPLIT
+                self.env[data_model].browse(ids).unlink()
+                # Commit this deletion to keep it even after a worker timeout
+                self.env.cr.commit()
+
+        self.env.cr.close()
         return True
