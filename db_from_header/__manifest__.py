c37b8475c89b263d3e55769ac7f09acd2c2f9c8b
Yann Papouin <ypa@decgroupe.com>
2022-12-19 14:46:34 +0100

    [MIG] db_from_header: Migration to 14.0

--- db_from_header/__manifest__.py
+++ db_from_header/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "db_from_header",
     "summary": "Pre-select a database with an HTTP header",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "website": "https://github.com/OCA/server-tools",
     "author": "DEC, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
